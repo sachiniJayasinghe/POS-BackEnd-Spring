7c661d0d78adaa774fafa3c6a58603f5ee935357
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-05 13:04:31 +0530

    Create Model mapper based utility for entity dto mapping and vice versa

---
+++ src/main/java/gdse68/posbackendspring/util/Mapping.java
@@ -0,0 +1,38 @@
+package gdse68.posbackendspring.util;
+
+import gdse68.posbackendspring.dto.CustomerDTO;
+import gdse68.posbackendspring.dto.OrdersDTO;
+import gdse68.posbackendspring.entity.Customer;
+import gdse68.posbackendspring.entity.Orders;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class Mapping {
+    @Autowired
+    private ModelMapper modelMapper;
+
+    //matters of NoteEntity and DTO
+    public OrdersDTO convertToDTO(Orders orders) {return modelMapper.map(orders, OrdersDTO.class);}
+    public Orders convertToEntity(OrdersDTO dto) {return modelMapper.map(dto, Orders.class);}
+    public List<OrdersDTO> convertToDTO(List<Orders> orders) {
+        return modelMapper.map(orders, new TypeToken<List<OrdersDTO>>() {}.getType());
+    }
+    //Customer matters mapping
+    public Customer convertToCustomerEntity(CustomerDTO customerDTO) {
+        return modelMapper.map(customerDTO, Customer.class);
+    }
+    public CustomerDTO  convertToUserDTO(Customer customer) {
+        return modelMapper.map(customer, CustomerDTO.class);
+    }
+    public List<CustomerDTO> convertUserToDTOList(List<Customer> customers) {
+        return modelMapper.map(customers, new TypeToken<List<CustomerDTO>>() {}.getType());
+    }
+
+
+
+}
