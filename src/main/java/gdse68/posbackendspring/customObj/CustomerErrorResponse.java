3ce7a6450eadc97df1e8be94eef9f193f1908cac
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-07 23:05:41 +0530

    Optimized the process of customer create with customErrorResponse

---
+++ src/main/java/gdse68/posbackendspring/customObj/CustomerErrorResponse.java
@@ -0,0 +1,14 @@
+package gdse68.posbackendspring.customObj;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CustomerErrorResponse implements CustomerResponse, Serializable {
+    private int errorCode;
+    private String errorMessage;
+}
