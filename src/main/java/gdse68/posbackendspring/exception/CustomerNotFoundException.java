f2bc56b1cb06871ab67d762b4cb4110ce162b661
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-07 22:20:26 +0530

    Optimized the process of customer create with custom exception handling

---
+++ src/main/java/gdse68/posbackendspring/exception/CustomerNotFoundException.java
@@ -0,0 +1,15 @@
+package gdse68.posbackendspring.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException() {
+    }
+
+    public CustomerNotFoundException(String message) {
+    }
+
+    public CustomerNotFoundException(String message, Throwable cause) {
+    }
+
+
+
+}
--- src/main/java/gdse68/posbackendspring/service/CustomerServiceImpl.java
+++ src/main/java/gdse68/posbackendspring/service/CustomerServiceImpl.java
@@ -3,6 +3,8 @@ package gdse68.posbackendspring.service;
 import gdse68.posbackendspring.customObj.CustomerResponse;
 import gdse68.posbackendspring.dao.CustomerDao;
 import gdse68.posbackendspring.dto.CustomerDTO;
+import gdse68.posbackendspring.entity.Customer;
+import gdse68.posbackendspring.exception.CustomerNotFoundException;
 import gdse68.posbackendspring.util.Mapping;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -29,6 +32,16 @@ public class CustomerServiceImpl implements CustomerService{
 
     @Override
     public void updateCustomer(CustomerDTO customerDTO) {
+        Optional<Customer> tmpUser = customerDao.findById(customerDTO.getCustomerId());
+        if(!tmpUser.isPresent()){
+            throw new CustomerNotFoundException("Customer not found");
+        }else {
+            tmpUser.get().setCustomerId(customerDTO.getCustomerId());
+            tmpUser.get().setName(customerDTO.getName());
+            tmpUser.get().setAddress(customerDTO.getAddress());
+            tmpUser.get().setSalary(customerDTO.getSalary());
+            tmpUser.get().setProfilePic(customerDTO.getProfilePic());
+        }
 
     }
 
