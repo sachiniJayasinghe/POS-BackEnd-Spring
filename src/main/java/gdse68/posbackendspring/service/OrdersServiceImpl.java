8cb5f9d2c292ebdf9535ff6679be5fee288e79fc
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-08 13:48:19 +0530

    Test the service with save customer

--- src/main/java/gdse68/posbackendspring/service/OrdersServiceImpl.java
+++ src/main/java/gdse68/posbackendspring/service/OrdersServiceImpl.java
@@ -1,13 +1,30 @@
 package gdse68.posbackendspring.service;
 
 import gdse68.posbackendspring.customObj.OrdersResponse;
+import gdse68.posbackendspring.dao.OrdersDao;
 import gdse68.posbackendspring.dto.OrdersDTO;
+import gdse68.posbackendspring.util.Mapping;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+@Service
+@Transactional
 
 public class OrdersServiceImpl implements OrdersService{
+
+    @Autowired
+    private OrdersDao ordersDao;
+    @Autowired
+    private Mapping mapping;
     @Override
     public void saveOrder(OrdersDTO ordersDTO) {
+        if (ordersDao.existsById(ordersDTO.getOderId())) {
+            throw new RuntimeException("Cannot data saved");
+        }
+        ordersDao.save(mapping.convertToEntity(ordersDTO));
 
     }
 
