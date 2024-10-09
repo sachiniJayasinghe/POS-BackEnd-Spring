01fd50447902094be939ba96a5fb39e338384890
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-09 21:12:42 +0530

    Tested save oders

--- src/main/java/gdse68/posbackendspring/controller/OrderController.java
+++ src/main/java/gdse68/posbackendspring/controller/OrderController.java
@@ -1,12 +1,38 @@
 package gdse68.posbackendspring.controller;
 
+import gdse68.posbackendspring.dto.OrdersDTO;
+import gdse68.posbackendspring.exception.DataPersistFailedException;
+import gdse68.posbackendspring.service.OrdersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/order")
+@RequestMapping("api/v1/orders")
 @RequiredArgsConstructor
 public class OrderController {
+    @Autowired
+    private final OrdersService ordersService;
 
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createOrders(@RequestBody OrdersDTO order) {
+        if (order == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        } else {
+            try {
+                ordersService.saveOrder(order);
+                return new ResponseEntity<>(HttpStatus.CREATED);
+            } catch (DataPersistFailedException e) {
+                return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            } catch (Exception e) {
+                return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
 }
