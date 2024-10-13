74797b205397b0c9e12afe723a5e977b609958b5
sachiniJayasinghe <gunasekarasachini2003@gmail.com>
2024-10-14 00:37:39 +0530

    Create the  PlaceOrder Controller

--- src/main/java/gdse68/posbackendspring/controller/PlaceOrderController.java
+++ src/main/java/gdse68/posbackendspring/controller/PlaceOrderController.java
@@ -1,6 +1,8 @@
 package gdse68.posbackendspring.controller;
 
+import gdse68.posbackendspring.service.OrdersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/order")
 @RequiredArgsConstructor
 public class PlaceOrderController {
+    @Autowired
+    private final OrdersService ordersService;
+
 }
